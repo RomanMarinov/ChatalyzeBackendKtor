a32ece7f7f52b60c4c233b0f52e596b1c6b0c2a7
romanmarinov <marinov37@mail.ru>
2023-09-23 23:04:30 +0300

    add sendEmail logic add javax.mail

---
+++ src/main/kotlin/ru/marinovdev/features/send_email/SendEmailToUser.kt
@@ -0,0 +1,80 @@
+package ru.marinovdev.features.send_email
+
+import java.util.*
+import java.util.concurrent.Executors
+import javax.mail.*
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
+class SendEmailToUser {
+
+    companion object {
+
+        fun sendEmail(
+            emailForSending: String,
+            password: String
+        ) {
+            val executorService = Executors.newSingleThreadExecutor()
+            executorService.execute {
+                try {
+                    val message: Message = createEmailMessage(
+                        emailForSending = emailForSending,
+                        password = password
+                    )
+                    Transport.send(message)
+                    handleSuccessCallback()
+                } catch (e: Exception) {
+                    handleFailureCallback(e)
+                }
+            }
+            executorService.shutdown() // Завершение работы ExecutorService
+        }
+
+        @Throws(MessagingException::class)
+        private fun createEmailMessage(emailForSending: String, password: String): Message {
+
+            val session = createSession()
+            val message: Message = MimeMessage(session)
+            message.subject = "Password to enter the Chatalyze app"
+            val htmlContent = "<html><head><meta charset='UTF-8'></head><body>" +
+                    "<p><strong>Hello my dear friend!</strong></p>" +
+                    "<p><strong>Password: </strong></p><h1 style='color: #008080;'>" + password + "</h1>" +
+                    "</body></html>"
+            message.setContent(htmlContent, "text/html; charset=utf-8")
+            val iAm = "marinov.dev.88@gmail.com"
+            val iAmY = "marinov37@mail.ru"
+            val address: Address = InternetAddress(emailForSending)
+            message.setRecipient(Message.RecipientType.TO, address)
+            return message
+        }
+
+        private fun createSession(): Session {
+            val properties = Properties()
+            properties["mail.smtp.auth"] = true
+            properties["mail.smtp.host"] = "smtp.gmail.com"
+            properties["mail.smtp.port"] = 587
+            properties["mail.smtp.starttls.enable"] = true
+            properties["mail.transport.protocol"] = "smtp"
+            properties["mail.smtp.ssl.protocols"] = "TLSv1.2"
+            return Session.getInstance(properties, object : Authenticator() {
+                override fun getPasswordAuthentication(): PasswordAuthentication {
+                    val name_passwords = "pnfhesmchfrqepii"
+                    val gmail = "chatalyze.help@gmail.com"
+                    val passwordGmail = "nanSag-japgyr-xorru5"
+                    return PasswordAuthentication(gmail, name_passwords)
+                }
+            })
+        }
+
+        private fun handleSuccessCallback() {
+            // Код обработки успешного обратного вызова
+            println("Письмо успешно отправлено! \nТак же рекомендуем проверить папку СПАМ")
+        }
+
+        private fun handleFailureCallback(e: java.lang.Exception) {
+            // Код обработки обратного вызова при ошибке
+            println("Возникла ошибка при отправке письма: " + e.message)
+        }
+
+    }
+}
---
+++ src/main/kotlin/ru/marinovdev/features/send_email/SenderEmailRouting.kt
@@ -0,0 +1,20 @@
+package ru.marinovdev.features.send_email
+
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+
+fun Application.configureSenderEmailRouting() {
+    try {
+        routing {
+            post("/password") {
+                val senderEmailController = SenderEmailController(call)
+                senderEmailController.sendEmail()
+            }
+        }
+    } catch (e: Exception) {
+        println("try catch senderEmailRouting e=" + e)
+    }
+
+}
